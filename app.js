b148c7620e19b9068d618bcbe85a3377bfab893a
baraahhableh <baraah.hableh@yahoo.com>
2021-08-04 17:02:58 +0300

    added lab07

--- app.js
+++ app.js
@@ -8,14 +8,9 @@ let locationArr = [];
 
 
 
-Stores.prototype.custNumPerHour = function(min, max) {
-    let numberOfCustomer = Math.floor(Math.random() * (max - min + 1) + min);
-    return numberOfCustomer
-}
-
 
 function Stores(name, minCust, maxCust, AverageCookie) {
-
+    
     this.name = name;
     this.minCust = minCust;
     this.maxCust = maxCust;
@@ -34,281 +29,77 @@ let lima = new Stores('lima', 2, 16, 4.6);
 console.log(locationArr);
 
 
+Stores.prototype.custNumPerHour = function(min, max) {
+    let numberOfCustomer = Math.floor(Math.random() * (max - min + 1) + min);
+    return numberOfCustomer
+}
+
 Stores.prototype.CustPerHour = function(){
     
     for (let i = 0; i < workingHours.length; i++) {
 
 
-        this.ArrayForCustomers.push(custNumPerHour(this.minCust, this.maxCust));
+        this.ArrayForCustomers.push(this.custNumPerHour(this.minCust, this.maxCust));
 
-        this.ArrayForCookies[i] = this.ArrayForCustomers[i] * this.AverageCookie
+        this.ArrayForCookies[i] = Math.floor(this.ArrayForCustomers[i] * this.AverageCookie)
     }
 
 }
 
 
-let div = 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let seattle = {
-    name: 'Seattle',
-    minCust: 23,
-
-    maxCust: 65,
-    AverageCookie: 6.3,
-
-    ArrayForCustomers: [],
-    ArrayForCookies: [],
-
-    CustPerHour: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-
-            this.ArrayForCustomers.push(custNumPerHour(this.minCust, this.maxCust));
-
-            this.ArrayForCookies[i] = this.ArrayForCustomers[i] * this.AverageCookie
-        }
-
-    },
-
-
-    createTheList: function () {
-
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-            let result = [workingHours, this.ArrayForCookies + ' cookies']
-
-            let li = document.createElement('li');
-
-            ul.appendChild(li);
-
-
-            li.textContent = result
-            document.write(result);
-        }
-
-    },
-
-}
-
-seattle.CustPerHour();
-
-seattle.createTheList();
-
-let tokyo = {
-    name: 'Tokyo',
-    minCust: 3,
-
-
-    maxCust: 24,
-    AverageCookie: 1.2,
-
-    ArrayForCustomers: [],
-    ArrayForCookies: [],
-
-    CustPerHour: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-            this.ArrayForCustomers.push(custNumPerHour(this.minCust, this.maxCust));
-
-            this.ArrayForCookies[i] = this.ArrayForCustomers[i] * this.AverageCookie
-        }
-
-    },
-
-
-    createTheList: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-            let result = [workingHours, (this.ArrayForCookies[i] + ' cookies')]
-
-            let li = document.createElement('li');
-
-            ul.appendChild(li);
-
-            li.textContent = result
-
-        }
-
-    },
-
-}
-
-tokyo.CustPerHour();
-
-tokyo.createTheList();
-
-
-
-let dubai = {
-    name: 'Dubai',
-    minCust: 11,
-
-    maxCust: 38,
-    AverageCookie: 3.7,
-
-
-    ArrayForCustomers: [],
-    ArrayForCookies: [],
-
-    CustPerHour: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-
-            this.ArrayForCustomers.push(custNumPerHour(this.minCust, this.maxCust));
-
-            this.ArrayForCookies[i] = this.ArrayForCustomers[i] * this.AverageCookie
-        }
-
-    },
-
-
-    createTheList: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-            let result = [workingHours, (this.ArrayForCookies[i] + ' cookies')]
-
-            let li = document.createElement('li');
-
-            ul.appendChild(li);
-
-            li.textContent = result
-
-        }
-
-
-    },
-
-
-
+let div = document.getElementById('cookies');
+let table = document.createElement('table');
+div.appendChild(table);
+
+let headRow = document.createElement('tr');
+table.appendChild(headRow);
+headRow.textContent = ' ';
+
+
+let th = document.createElement('th');
+
+
+
+
+Location.prototype.renderTable = function () {
+    
+    let firstRow = document.createElement('tr');
+    table.appendChild(firstRow);
+    firstRow.textContent = this.name;
+    
+    for (let i = 0; i < workingHours.length; i++) {
+        let td = document.createElement('td');
+        firstRow.appendChild(td);
+        td.textContent = this.ArrayForCookies[i];
+    }
+    
+  
+    
+    headRow.appendChild(th);
+    th.textContent = 'Daily Location Total';
+    
+    
+    
 }
 
 
-dubai.CustPerHour();
 
-dubai.createTheList();
-
-
-
-let paris = {
-
-    name: 'Paris',
-    minCust: 20,
-
-    maxCust: 38,
-    AverageCookie: 2.3,
-
-    ArrayForCustomers: [],
-    ArrayForCookies: [],
-
-
-    CustPerHour: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-            this.ArrayForCustomers.push(custNumPerHour(this.minCust, this.maxCust));
-
-            this.ArrayForCookies[i] = this.ArrayForCustomers[i] * this.AverageCookie
-        }
-
-    },
-
-
-    createTheList: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-
-            let result = [workingHours, (this.ArrayForCookies[i] + ' cookies')]
-
-            let li = document.createElement('li');
-
-            ul.appendChild(li);
-
-            li.textContent = result
-
-        }
-
-    },
-
-}
-
-paris.CustPerHour();
-
-paris.createTheList();
+function header() {
+    
+    for (let i = 0; i < workingHours.length; i++) {
+        let th1 = document.createElement('th');
+        headRow.appendChild(th1);
+        th1.textContent = workingHours[i];     
+        
+    }
+    }
+    header();
 
 
 
-let lima = {
-    name: 'Lima',
-    minCust: 2,
-
-    maxCust: 16,
-    AverageCookie: 4.6,
-
-
-    ArrayForCustomers: [],
-    ArrayForCookies: [],
-
-
-    CustPerHour: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-            this.ArrayForCustomers.push(custNumPerHour(this.minCust, this.maxCust));
-
-
-            this.ArrayForCookies[i] = this.ArrayForCustomers[i] * this.AverageCookie
-        }
-
-    },
-
-
-    createTheList: function () {
-
-        for (let i = 0; i < workingHours.length; i++) {
-
-            let result = [workingHours, (this.ArrayForCookies[i] + ' cookies')]
-
-            let li = document.createElement('li');
-
-
-            ul.appendChild(li);
-
-
-            li.textContent = result
-
-        }
-
-
-    },
-
-
-
-}
-
-lima.CustPerHour();
-
-
-lima.createTheList();
-
-
+    for (let i = 0; i < locationArr.length; i++) {
+        locationArr[i].CustPerHour();
+        locationArr[i].renderTable();
+    }
+    // seattle.renderTable();
 
